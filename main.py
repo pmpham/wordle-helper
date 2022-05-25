8a1602129e076ab272a086782b2488aaa599a853
pmpham <petermphamm@gmail.com>
2022-05-24 21:39:18 -0700

    added exit to main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import helperV1, helperV2
 
 choice = 0
-while choice < 1 or choice >2:
+while choice < 1 or choice >3:
   try:
-    choice = int(input("Choose a version to play\n1. New\n2. Old\n"))
+    choice = int(input("Choose a version to play\n1. New\n2. Old\n3. Exit\n"))
   except:
     print("Invalid Input")
 if choice ==1:
   helperV2.menu()
-if choice ==2:
+elif choice ==2:
   helperV1.menu()
+elif choice ==3:
+  pass
